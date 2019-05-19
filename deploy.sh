6412789dff5d87b385a799f0e395c1de80c41e1c
alvinyzhou <zhouyajsj13@163.com>
2019-05-19 17:40:32 +0800

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+rm -r -f /home/ducex/apache-tomcat-8.5.23/webapps/comapp-0.0.1-SNAPSHOT.war
+
+cp -f /root/.jenkins/workspace/demo/ci-cd-demo/target/comapp-0.0.1-SNAPSHOT.war /home/ducex/apache-tomcat-8.5.23/webapps/comapp-0.0.1-SNAPSHOT.war
