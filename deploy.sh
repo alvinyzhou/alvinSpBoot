94dfbe9ef36c12a33e262127fe0ce12b4dfa11f9
alvinyzhou <zhouyajsj13@163.com>
2019-05-19 19:24:28 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+sudo -i | Digital@2018
 rm -r -f /home/ducex/apache-tomcat-8.5.23/webapps/comapp-0.0.1-SNAPSHOT.war
 
 cp -f /root/.jenkins/workspace/demo/ci-cd-demo/target/comapp-0.0.1-SNAPSHOT.war /home/ducex/apache-tomcat-8.5.23/webapps/comapp-0.0.1-SNAPSHOT.war
